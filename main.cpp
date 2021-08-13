a0ecaa5cb52a1a25de7f22c4263655ebd10749ca
Daniela701 <80079229+Daniela701@users.noreply.github.com>
2021-08-13 12:58:42 -0500

    Problema9

--- main.cpp
+++ main.cpp
@@ -1,36 +1,35 @@
 #include <iostream>
 using namespace std;
 
-/*Programa que recibe una cadena de caracteres y separa las letras de los numeros
- en dos cadenas diferentes
+/*Programa que recibe un numero n y lee una cadena de caracteres y las separa en n cifras
+ e imprime la suma de los numeros
  */
 
 int main()
 {
-    char cadena[50], tex[50], nums[50];//cadenas que almacenan las entradas y salidas
-    char numeros[11]={48,49,50,51,52,53,54,55,56,57};//Representacion de los numeros del 0-9 en ascii
-    int cont=0, c=0;
-    bool det=true;
-    cout << "Escriba una cadena de letras y numeros: " << endl;
+    char cadena[50];
+    int n=0, suma=0, contador, numero, cadnum[1];
+    cout<<"Escriba la cadena de numeros : "<<endl;
     cin.getline(cadena,50);
 
+    cout << "Escriba un numero : " << endl;
+    cin >>n;
+
     cout<<"Original: "<<cadena<<endl;
+    int size= sizeof(cadena);
+    size/=n;
+
     for (int i=0; i<50; i++){
-        det=true;
-        for (int l=0; l<10; l++){
-            if (cadena[i]==numeros[l]){//Se evalua si en la cadena hay numeros
-                 nums[c]=cadena[i];//Se agregan los numeros a la cadena de salida
-                 c++;
-                 det=false;
-                 break;
+        contador=0;
+        if(size % 2==0){
+            while (contador!=n){
+                cadnum[0]+=cadena[i];
+                i++;
+                contador++;
             }
-                 }
-        if(det){
-              tex[cont]=cadena[i];//Se agregra el texto a la cadena de salida
-              cont++;
-             }
- }
-    cout<<"Texto: "<<tex<<endl;
-    cout<<"Numero: "<<nums<<endl;
-    return 0;
+        }
+        else{
+
+        }
+  }
 }
